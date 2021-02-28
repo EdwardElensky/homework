02dd2c33c061d2bb18f3eb87b8c39a193b6993c2
Ed Elensky <elenskyed516@gmail.com>
2021-02-28 20:39:29 +0300

    edit download-dialog

--- task2/ya-fin-guru.sh
+++ task2/ya-fin-guru.sh
@@ -2,31 +2,27 @@
 
 #help
 if [ "$1" = "-h" ]; then
-
-	echo "Hello! This script work with data from https://yandex.ru/news/quotes/ .
+echo "Hello! This script work with data from https://yandex.ru/news/quotes/ .
 If you allow it can download data and tell you which March the price was the least volatile since 2015.
-To do so you'll have to find the difference between MIN and MAX values for the period.
-	exit 0
-
+To do so you'll have to find the difference between MIN and MAX values for the period."
+exit 0
 fi
 
 # it is plan
 echo "Hello!"
 # 0. check  quotes.json
-if [ -f ./quotes.json ]; then
-echo "Файл существует!"
+if [ -r ./quotes.json ]; then
+echo "Cool! Файл существует!"
+echo "..."
 else
-echo "Файл not существует! :-)"
+echo "Hmm... Файл нe существует! :-)"
+while true; do
+    read -p "Do you wish to download this file?(Y/N) " yn
+    case $yn in
+        [Yy]* ) curl -s https://yandex.ru/news/quotes/graph_2000.json > ./quotes.json; break;;
+        [Nn]* ) echo "Hasta la vista "; exit;;
+        * ) echo "Please answer (Y)yes or (N)no. ";;
+    esac
+done
 fi
 
-# 1. if no - Ask the user for download
-# 1.1 - if user say yes - curl it and go 2.
-# 1.2 - if user say no - echo bye and exit
-# 2. magic
-
-read -p 'Ask User: ' useranswer
-
-echo $useranswer
-
-# 1.1. download data
-# curl -s https://yandex.ru/news/quotes/graph_2000.json > ./quotes.json
