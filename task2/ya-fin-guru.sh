21a9db0e5c0e33f09a7ce9e3f7a14faaa30bc0af
Ed Elensky <elenskyed516@gmail.com>
2021-02-28 17:43:56 +0300

    TASK-02: add check file quotes.json. it work

--- task2/ya-fin-guru.sh
+++ task2/ya-fin-guru.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+#help
+
+
 # it is plan
+echo "Hello!"
 # 0. check  quotes.json
+if [ -f ./quotes.json ]; then
+echo "Файл существует!"
+else
+echo "Файл not существует! :-)"
+fi
+
 # 1. if no - Ask the user for download
 # 1.1 - if user say yes - curl it and go 2.
 # 1.2 - if user say no - echo bye and exit
 # 2. magic
 
 read -p 'Ask User: ' useranswer
+
+echo $useranswer
 
 # 1.1. download data
 # curl -s https://yandex.ru/news/quotes/graph_2000.json > ./quotes.json
