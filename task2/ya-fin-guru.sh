861d51a0f804e0b3ee1fc4f70e1690f4759f61b0
Ed Elensky <elenskyed516@gmail.com>
2021-03-03 10:43:03 +0300

    add awk split value to month-year files (it work),cleaned code

--- task2/ya-fin-guru.sh
+++ task2/ya-fin-guru.sh
@@ -4,47 +4,23 @@
 if [ "$1" = "-h" ]; then
 echo "Hello! This script work with data from https://yandex.ru/news/quotes/ .
 If you allow it can download data and tell you which March the price was the least volatile since 2015.
-To do so you'll have to find the difference between MIN and MAX values for the period."
-exit 0
+To do so you'll have to find the difference between MIN and MAX values for the period.
+Script need from you number of month for calculate. Example - ya-fin-guru.sh 05"
+exit
 fi
 
-echo "Hello!"
-
-if [ -r ./quotes.json ]; then
-echo "The data file is exist. Good. We migth work..."
-
-raw_date_tab=$(jq -c '.prices[][0]' quotes.json | awk '{$1 = $1/1000} {print $1}' )
-
-for dat in $raw_date_tab;
-do
-date_tab+=$(date  -d @$dat  "+%m-%Y%t")
-done
-dat_tab=$(echo $date_tab | sed 's/\s\+/\n/g')
-
-val_tab=$(jq -c '.prices[][1]' quotes.json | awk '{print $1}' )
-
-# ------ it working! do not edit!
-#dvt=$(paste <(echo "$dat_tab") <(echo "$val_tab"))
-#echo "$dvt"
-#echo "Data filtering..."
-
-
- join <(echo "$dat_tab") <(echo "$val_tab")
-
-# -------------------  not work
-#dvt=$(join --header --nocheck-order <<<((${date_tab[*]})) <<<((${val_tab[*]})))
-#echo "++++++ $dvt"
-
-#date_val_tab=($(echo "${date_tab[*]}") $(echo "${val_tab[*]}"))
-#echo "$date_val_tab"
-
-# ---- wrong way
-#new=("${date_tab[@]}" "${val_tab[@]}")
-#echo ${new[@]}
-
-
+if (echo "$1" | grep -E -q "^(0[1-9]|1[0-2])$");
+then
+	month=$1
 else
-echo "Hmm... File does not exist! :-)"
+	month="03"
+  echo "You not set correct month. I will calculate values for marth."
+fi
+echo "Hello! Calculating for "$month"-th month."
+
+
+if [ ! -r ./quotes.json ]; then
+echo "Hmm... Json file does not exist! :-)"
 while true; do
     read -p "Do you wish to download this file?(Y/N) " yn
     case $yn in
@@ -53,5 +29,50 @@ while true; do
         * ) echo "Please answer (Y)yes or (N)no. ";;
     esac
 done
+
+
+else
+echo "The json file is exist. Good. We will work..."
+sleep 1s
+
+echo "get dates from file..."
+raw_date_tab=$(jq -c '.prices[][0]' quotes.json | awk '{$1 = $1/1000} {print $1}' )
+
+
+echo "convert dates..."
+for dat in $raw_date_tab;
+do
+date_tab+=$(date  -d @$dat  "+%m-%Y%t")
+done
+dat_tab=$(echo $date_tab | sed 's/\s\+/\n/g' | sed 's/-\+/ /g' )
+
+
+echo "get values from file..."
+val_tab=$(jq -c '.prices[][1]' quotes.json | awk '{print $1}' )
+
+
+echo "merge dates and values..."
+dvt=$(paste <(echo "$dat_tab") <(echo "$val_tab"))
+
+
+echo "Data filtering..."
+filt_m=$(grep "$month " <(echo "$dvt"))
+
+# echo "$filt_m"
+
+awk '{ print $3 >>($1"-"$2); close(1$"-"$2) }' <(echo "$filt_m")
+
+# берется среднее значение курса за март. 
+# Затем, проверяется разность между средним значением и  минимальным и максимальным курсом за период (март). 
+# И далее выбирается год, в который в марте разница была минимальна.
+# mean — среднее
+# max — максимальное
+# min — минимальное
+# volatility = ((mean - min) + (max - mean))/2
+
+
+
+
 fi
 
+exit
