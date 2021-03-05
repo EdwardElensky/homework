9dc01f7c2d453dc5b2572de45f5e9043e5d230ac
Ed Elensky <elenskyed516@gmail.com>
2021-03-05 12:06:11 +0300

    add list of month and counter of month from var

--- task2/ya-fin-guru.sh
+++ task2/ya-fin-guru.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-
+# ==============================
 if [ "$1" = "-h" ]; then
 echo "Hello! This script work with data from https://yandex.ru/news/quotes/ .
 If you allow it can download data and tell you which March the price was the least volatile since 2015.
@@ -8,7 +7,7 @@ To do so you'll have to find the difference between MIN and MAX values for the p
 Script need from you number of month for calculate. Example - ya-fin-guru.sh 05"
 exit
 fi
-
+# ==============================
 if (echo "$1" | grep -E -q "^(0[1-9]|1[0-2])$");
 then
 	month=$1
@@ -17,8 +16,7 @@ else
   echo "You not set correct month. I will calculate values for marth."
 fi
 echo "Hello! Calculating for "$month"-th month."
-
-
+# ==============================
 if [ ! -r ./quotes.json ]; then
 echo "Hmm... Json file does not exist! :-)"
 while true; do
@@ -29,53 +27,57 @@ while true; do
         * ) echo "Please answer (Y)yes or (N)no. ";;
     esac
 done
-
-
+# ==============================
 else
 echo "The json file is exist. Good. We will work..."
 TMPDIR=".${0##*/}-$$" && mkdir -v "$TMPDIR"
-sleep 1s #ha ha ha
-ls -ali $TMPDIR/
-
+# sleep 1s #ha ha ha
+# ==============================
 echo "get dates from file..."
 raw_date_tab=$(jq -c '.prices[][0]' quotes.json | awk '{$1 = $1/1000} {print $1}' )
-
-
-echo "convert dates..."
+# ==============================
+# echo "convert dates..."
 for dat in $raw_date_tab;
 do
 date_tab+=$(date  -d @$dat  "+%m-%Y%t")
 done
 dat_tab=$(echo $date_tab | sed 's/\s\+/\n/g' | sed 's/-\+/ /g' )
-
-
+# ==============================
 echo "get values from file..."
 val_tab=$(jq -c '.prices[][1]' quotes.json | awk '{print $1}' )
-
-
+# ==============================
 echo "merge dates and values..."
 dvt=$(paste <(echo "$dat_tab") <(echo "$val_tab"))
-
-
-echo "Data filtering..."
+# ==============================
+echo "data filtering..."
 filt_m=$(grep "$month " <(echo "$dvt"))
+# ==============================
+echo "separate data by month"
+separate_month=$(awk -v y="$TMPDIR" '{print y ; print $3 >>(y"/"$1"_"$2); close(y"/"1$"_"$2) }' <(echo "$filt_m"))
 
-# echo "$filt_m"
+list_of_month=$(ls "$TMPDIR")
+# echo "$list_of_month"
 
 
-awk -v '{ print $3 >>($TMPDIR/$1"-"$2); close($TMPDIR/1$"-"$2) }' <(echo "$filt_m")
+#for var in "$list_of_month";
+#do
+count=$(grep _ -c <(echo "$list_of_month"))
+echo "$count"
+#var=$("$TMPDIR"/var)
+#echo "$var"
+#done
 
-ls -ali $TMPDIR/
 
-# awk -v m=$[i*2-1] 'FNR == m {print $2}'
 
+# ==============================
+rm -r $TMPDIR
 # берется среднее значение курса за март. 
 # Затем, проверяется разность между средним значением и  минимальным и максимальным курсом за период (март). 
 # И далее выбирается год, в который в марте разница была минимальна.
 # mean — среднее
 # max — максимальное
 # min — минимальное
-# volatility = ((mean - min) + (max - mean))/2
+# volatility = ((mean - min) + (max - mean))/2  -eq  (max-min)/2
 
 
 
